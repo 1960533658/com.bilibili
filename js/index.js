bcc60d8596ddb8dfddfae283faa01199c2c541b7
196533658 <1960533658@qq.com>
2019-12-09 23:08:37 +0800

    提交:2019.12.09第一次项目

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// var radius_bg = document.querySelectorAll('.radius span');
+// for (var i = 0; i < radius_bg.length; i++) {
+//     radius_bg[i].onclick = function () {
+//         this.style.backgroundImg = '../images/slideshow.png';
+//     }
+// }
