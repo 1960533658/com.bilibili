f4ddd0df54e2f47c3790c7ea83421d69f3255784
196533658 <1960533658@qq.com>
2020-04-23 21:45:16 +0800

    提交:2020-04-23次jQuery项目

--- PC端B站/js/index.js
+++ PC端B站/js/index.js
@@ -1,6 +1,28 @@
-// var radius_bg = document.querySelectorAll('.radius span');
-// for (var i = 0; i < radius_bg.length; i++) {
-//     radius_bg[i].onclick = function () {
-//         this.style.backgroundImg = '../images/slideshow.png';
-//     }
-// }
+// 悬浮导航
+let arr = []
+$('.box>div').eq(4).nextAll().each(function (i, n) {
+    if ($(this).hasClass("advertising")) {
+        return true;
+    }
+    arr[arr.length] = $(this).offset().top - 0.921875;
+});
+
+$(window).on('scroll', function () {
+    for (let i = 0; i < arr.length; i++) {
+        if ($(window).scrollTop() >= arr[i]) {
+            $('.list-box div div').eq(i).css('background-color', 'rgb(0, 161, 214)').siblings().css('background-color', 'white')
+        }
+    }
+})
+
+// 事件委托未完成
+// $('.list-box>div').on('click', $('.list-box div div'), function () {
+
+//     $(window).animate({ scrollTop: $('.box>div').eq($(this).index() + 5).scrollTop() }, 1000)
+// })
+
+$('.list-box div div').on('click', function () {
+
+    $(window).scrollTop($('.scroll').eq($(this).index() + 5).offset().top)
+})
+// BUG: 当从下往上滚动时偶尔无法自动选中直播这一行  位置:悬浮导航右侧
